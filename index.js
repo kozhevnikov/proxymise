a4a4c16d29c48bbeefe05c6c7aaaa2fc5b7a2c41
Ilya Kozhevnikov <git@kozhevnikov.com>
2018-07-13 13:30:53 +0100

    Clean up

--- index.js
+++ index.js
@@ -1,39 +1,28 @@
 const proxymise = (target) => {
-  switch (typeof target) {
-    case 'function': {
-      return new Proxy(target, handler);
-    }
-    case 'object': {
-      const proxy = () => target;
-      proxy.__proxy__ = true;
-      return new Proxy(proxy, handler);
-    }
-    default: {
-      return target;
-    }
+  if (typeof target === 'object') {
+    const proxy = () => target;
+    proxy.__proxy__ = true;
+    return new Proxy(proxy, handler);
   }
+  return typeof target === 'function' ? new Proxy(target, handler) : target;
 };
 
 const handler = {
   /** @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy/handler/get */
   get(target, property, receiver) {
     if (target.__proxy__) target = target();
-
     if (property !== 'then' && property !== 'catch' && typeof target.then === 'function') {
       return proxymise(target.then(value => get(value, property, receiver)));
     }
-
     return proxymise(get(target, property, receiver));
   },
 
   /** @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy/handler/apply */
   apply(target, thisArg, argumentsList) {
     if (target.__proxy__) target = target();
-
     if (typeof target.then === 'function') {
       return proxymise(target.then(value => Reflect.apply(value, thisArg, argumentsList)));
     }
-
     return proxymise(Reflect.apply(target, thisArg, argumentsList));
   }
 };
