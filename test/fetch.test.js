3ffe113c93ff5e52925ff0b36d9a6102a78a0af0
Ilya Kozhevnikov <git@kozhevnikov.com>
2018-07-14 02:53:16 +0100

    Refactor fetch test

--- test/fetch.test.js
+++ test/fetch.test.js
@@ -1,18 +1,20 @@
-const fetch = require('..')(require('node-fetch'));
-const fetch2 = require('node-fetch');
+const proxymise = require('..');
+
+const fetch = require('node-fetch');
+const fetch2 = proxymise(require('node-fetch'));
 
 describe('fetch', () => {
   const url = 'https://api.jsonbin.io/b/5b47e175efaed72daef1ca4d';
 
-  it('should fetch with proxymise', async () => {
-    const value = await fetch(url).json().foo.bar;
+  it('should fetch without proxymise', async () => {
+    const response = await fetch(url);
+    const json = await response.json();
+    const value = json.foo.bar;
     expect(value).toBe('baz');
   });
 
-  it('should fetch without proxymise', async () => {
-    const response = await fetch2(url);
-    const json = await response.json();
-    const value = json.foo.bar;
+  it('should fetch with proxymise', async () => {
+    const value = await fetch2(url).json().foo.bar;
     expect(value).toBe('baz');
   });
 });
